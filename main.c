1ba989abeb9d5dea48703e2663770af643480de7
Matheus Yuji Morishita <matheusmorishita@hotmail.com>
2024-03-28 11:25:30 -0300

    changed the program entry

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "gauss_seidel.h"
 
 int main () {
-    double **A;
+    /*double **A;
     double *b, *a, *c, *d, *x;
     int cont = 0;
 
@@ -17,11 +17,15 @@ int main () {
 
     x = get_vetor(size);
     x = zera_vetor (x, size);
-   
 
     read_sistema_linear(A, b, size);
     vetor_triangular(A, a, c, d, size);
+    double time = timestamp ();
     cont = gauss_seidel_tridiagonal (d, a, c, b, x, size);
+    time = timestamp () - time;
+    printf ("%lf\n", time);*/
+
+
     //print_vetor (x, size);
     //cont = gauss_seidel(A, b, x, size);
 
@@ -44,7 +48,7 @@ int main () {
     //print_vetor(a, size-1);
     //print_vetor(c, size-1);
 
-
+    /*
     printf("\n");
     print_vetor(x, size);
     printf ("\n");
@@ -56,5 +60,41 @@ int main () {
     free_vetor(a);
     free_vetor(c);
     free_vetor(d);
-    free_vetor(x);
+    free_vetor(x);*/
+  
+    unsigned int size;
+    while (!feof(stdin)) {
+        scanf ("%d", &size);
+        double** A = get_matriz(size);
+        double* b = get_vetor(size);
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size+1; j++) {
+                if (j == size)
+                    scanf ("%lf", &b[i]);
+                else
+                    scanf ("%lf", &A[i][j]);
+        }
+
+        
+        print_matriz (A, size);
+        print_vetor (b, size);
+        printf("\n");
+
+
+        double* x = get_vetor(size);
+        double time = timestamp();
+        eliminacao_gauss (A, b, size);
+        retrossubs(A, b, x, size);
+        time = timestamp () - time;
+
+        print_vetor(x, size);
+        printf ("%lf\n", time);
+        printf("\n");
+
+        free_matriz(A, size);
+        free_vetor(b);
+        free_vetor(x);
+        size = 0;
+    }
 }
